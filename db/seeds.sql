80dae221600054c31c878f54df41eca314bb7961
Sohaib Jamil <sohaibjamil2012@gmail.com>
2018-11-21 09:09:29 -0500

    first

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('the bomb');
+INSERT INTO burgers (burger_name) VALUES ('the sleeper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('no rest', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('the eat', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('WHAT?!', true);
